e2ab3913fcdc24cd291ae955f88352a4a8ac9e5e
badytouray <badytouray@gmail.com>
2023-01-23 19:16:34 +0000

    removed error.log

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,4 +1,3 @@
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { Button } from 'react-bootstrap'
 import * as s from './HeroSection.module.scss'
